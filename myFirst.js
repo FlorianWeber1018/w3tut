a4aefa00e14b471871e8bea25caff24ec59b4fd6
Florian Weber <florian.weber@live.de>
2018-05-27 14:42:32 +0200

    nomsg

--- myFirst.js
+++ myFirst.js
@@ -4,6 +4,5 @@ var dt = require('./myFirstModule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDateTime());
-    res.end();
+    res.end('Hello World');
 }).listen(8080);
